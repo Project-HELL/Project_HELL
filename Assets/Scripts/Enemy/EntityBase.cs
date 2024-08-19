2112ba05e75cc3dfdd5ba71cd4ba499e11e11ffc
Cupelt <guswnd8800@naver.com>
2024-08-19 20:09:04 +0900

    수정: 공격 인터페이스의 구현불가

--- Assets/Scripts/Enemy/EntityBase.cs
+++ Assets/Scripts/Enemy/EntityBase.cs
@@ -1,7 +1,8 @@
+using System.Security.Cryptography.X509Certificates;
 using UnityEditor;
 using UnityEngine;
 
-public abstract class EntityBase : MonoBehaviour, Attackable {
+public abstract class EntityBase : MonoBehaviour {
 
     Rigidbody2D rigid;
 
@@ -17,13 +18,17 @@ public abstract class EntityBase : MonoBehaviour, Attackable {
     }
 
     // 기본적으로 닿으면 공격당함.
-    public void Attack() {
-
+    public void PerformAttack() {
+        if (this is Attackable) {
+            ((Attackable)this).Attack();
+        } else {
+            // Default Attack logic
+        }
     }
 
     private void Update() {
 
-        Attack();
+        PerformAttack();
 
         Vector2 movePos = transform.position + (Vector3)MoveAI();
         if (!(this is Falldownable)) {
