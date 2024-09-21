b9eed62371f5b344991cf497fd417ee48ea35c6b
Cupelt <guswnd8800@naver.com>
2024-09-21 23:35:25 +0900

    fix: sync classname && intuitively classname

--- Assets/Scripts/BonePlartform.cs
+++ Assets/Scripts/BonePlartform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class BoneBlockFade : MonoBehaviour
+public class BonePlatform : MonoBehaviour
 {
     private Tilemap tilemap;
     private TilemapRenderer tilemapRenderer;
